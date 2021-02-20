bd1fae3e714821465a92a1f7abcf0acf5406c6dd
balalior <huankaichen1983@gmail.com>
2021-02-20 00:55:28 +0000

    Init commit

---
+++ gamestonk/src/HelloWorld.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+
+// const HelloWorld = (props) => (
+//   <div>
+//     <h2>Hello <em>{props.name}</em></h2>
+//   </div>
+// );
+
+// function HelloWorld(props) {
+//   return (
+//     <div>
+//       <h2>Hello World! <em>{props.name}</em></h2>
+//     </div>
+//   );
+// }
+
+class HelloWorld extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Hello <em>{this.props.name}</em></h2>
+        </div>
+    )
+  }
+}
+
+export default HelloWorld;
